ccbffc1166606b353e4b8ac60932a09733a566d1
MrJavad <seydi.birjand@gmail.com>
2021-07-25 14:53:23 +0430

    livewire datatable

--- app/Http/Livewire/Admin/StatisticsDatatable.php
+++ app/Http/Livewire/Admin/StatisticsDatatable.php
@@ -3,16 +3,32 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Enrol;
+use Mediconesystems\LivewireDatatables\Column;
+use Mediconesystems\LivewireDatatables\DateColumn;
+use Mediconesystems\LivewireDatatables\TimeColumn;
+use Mediconesystems\LivewireDatatables\NumberColumn;
+use Mediconesystems\LivewireDatatables\BooleanColumn;
 use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
+
 
 class StatisticsDatatable extends LivewireDatatable
 {
     public $model = Enrol::class;
     public $exportable = true;
+    public $sort = 0;
     public $searchable = "exam_id";
     public function builder()
     {
-        return  Enrol::query()->where('exam_id',$this->params)->with(['user','exam']);
+        return  Enrol::query()->where('exam_id',$this->params);
     }
 
+    public function columns()
+    {
+        return [
+          NumberColumn::name('id')->label('شناسه'),
+          Column::name('exam.title')->label('نام آزمون')->filterable(),
+          Column::name('user.name')->label('نام کاربر')->filterable(),
+          Column::name('user.national_id')->label('کدملی کاربر')->filterable()
+        ];
+    }
 }
